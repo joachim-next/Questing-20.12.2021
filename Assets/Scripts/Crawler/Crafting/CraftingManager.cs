7a9fe4448ddb1b5c4ce232c1bc381cb062428435
Joachim Mero <joachim.mero@nextgames.com>
2021-12-20 10:19:09 +0200

    Make all tests pass

--- Assets/Scripts/Crawler/Crafting/CraftingManager.cs
+++ Assets/Scripts/Crawler/Crafting/CraftingManager.cs
@@ -2,15 +2,20 @@
 {
     public class CraftingManager
     {
+        private readonly ICraftingInventory _craftingInventory;
         private readonly ICraftingFormationValidator _formationValidator;
         
         public CraftingManager(ICraftingInventory craftingInventory, ICraftingFormationValidator formationValidator)
         {
+            _craftingInventory = craftingInventory;
             _formationValidator = formationValidator;
         }
         
         public CraftingResult TryCraft()
         {
+            if (!_craftingInventory.HasItems)
+                return CraftingResult.Failed;
+            
             return _formationValidator.FindFormations() ? CraftingResult.Success : CraftingResult.Failed;
         }
     }
