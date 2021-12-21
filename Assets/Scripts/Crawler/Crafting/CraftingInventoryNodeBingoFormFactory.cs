404f8c0a8f6de33d8e9d08247cfad38a2fc2b38d
Joachim Mero <joachim.mero@nextgames.com>
2021-12-21 08:50:12 +0200

    Add CraftingInventoryNodeBingoFormFactory

---
+++ Assets/Scripts/Crawler/Crafting/CraftingInventoryNodeBingoFormFactory.cs
@@ -0,0 +1,10 @@
+namespace Crawler.Crafting
+{
+    public class CraftingInventoryNodeBingoFormFactory
+    {
+        public CraftingInventoryNodeBingoForm[] Create(CraftingInventory inventory, CraftingFormation[] formations)
+        {
+            return new CraftingInventoryNodeBingoForm[0];
+        }
+    }
+}
---
+++ Assets/Scripts/Crawler/Crafting/Tests/CraftingInventoryNodeBingoFormFactoryTests.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Crawler.Crafting.Tests
+{
+    public class CraftingInventoryNodeBingoFormFactoryTests
+    {
+        private CraftingInventoryNodeBingoFormFactory _bingoFormFactory;
+        private CraftingInventory _inventory;
+        private CraftingFormation[] _formations;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _bingoFormFactory = new CraftingInventoryNodeBingoFormFactory();
+            
+            var inventoryNodes = new List<CraftingInventoryNode>
+            {
+                new CraftingInventoryNode(0),
+                new CraftingInventoryNode(1),
+                new CraftingInventoryNode(1),
+                new CraftingInventoryNode(1),
+                new CraftingInventoryNode(2),
+                new CraftingInventoryNode(2),
+            };
+            _inventory = new CraftingInventory(inventoryNodes);
+            
+            var firstFormationNodes = new[]
+            {
+                new CraftingFormationNode(0),
+                new CraftingFormationNode(1)
+            };
+
+            var secondFormationNodes = new[]
+            {
+                new CraftingFormationNode(2),
+                new CraftingFormationNode(2)
+            };
+
+            var thirdFormationNodes = new[]
+            {
+                new CraftingFormationNode(1),
+                new CraftingFormationNode(1)
+            };
+
+            _formations = new []
+            {
+                new CraftingFormation(firstFormationNodes), 
+                new CraftingFormation(secondFormationNodes), 
+                new CraftingFormation(thirdFormationNodes) 
+            };
+        }
+        
+        [Test]
+        public void Given_EmptyCraftingInventory_When_Create_Then_ReturnsEmpty()
+        {
+            var bingoForms = _bingoFormFactory.Create(_inventory, _formations);
+            
+            Assert.IsEmpty(bingoForms);
+        }
+    }
+}
