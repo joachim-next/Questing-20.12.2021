ee6449fa51a74b1c79793e0c4e7f1cbcded9b5af
Joachim Mero <joachim.mero@nextgames.com>
2021-12-22 09:05:00 +0200

    Only open CraftingView in Bootstrapper

--- Assets/Scripts/Crawler/UI/Tests/BootstrapperTests.cs
+++ Assets/Scripts/Crawler/UI/Tests/BootstrapperTests.cs
@@ -1,39 +1,6 @@
-﻿using Crawler.UI.Crafting;
-using DefaultNamespace;
-using NSubstitute;
-using NUnit.Framework;
-using UnityEngine;
-
-namespace Crawler.UI.Tests
+﻿namespace Crawler.UI.Tests
 {
     public class BootstrapperTests
     {
-        [Test]
-        public void When_Start_CraftingGridPresenterPresentCalled()
-        {
-            var craftingGridPresenter = Substitute.For<ICraftingGridPresenter>();
-
-            IocContainer.RegisterSingleton(craftingGridPresenter);
-            
-            var bootstrapper = new GameObject("Bootstrapper")
-                .AddComponent<Bootstrapper>();
-            
-            bootstrapper.Start();
-
-            craftingGridPresenter
-                .Received()
-                .Present();
-        }
-
-        [Test]
-        public void When_Awake_Then_CraftingGridPresenterToIocContainer()
-        {
-            var bootstrapper = new GameObject("Bootstrapper")
-                .AddComponent<Bootstrapper>();
-            
-            bootstrapper.Awake();
-            
-            Assert.DoesNotThrow(()=> IocContainer.GetSingleton<ICraftingGridPresenter>());
-        }
     }
 }
