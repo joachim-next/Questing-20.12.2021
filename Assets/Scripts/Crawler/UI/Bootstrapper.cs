923a3ef94d766ab594fbc4b0c30a0356bbcc7006
Joachim Mero <joachim.mero@nextgames.com>
2021-12-22 09:27:22 +0200

    Register CraftingInventory to IocContainer on bootstrapper awake

--- Assets/Scripts/Crawler/UI/Bootstrapper.cs
+++ Assets/Scripts/Crawler/UI/Bootstrapper.cs
@@ -1,3 +1,4 @@
+using Crawler.Crafting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,24 @@ namespace DefaultNamespace
 {
     public class Bootstrapper : MonoBehaviour
     {
+        public void Awake()
+        {
+            var craftingInventory = GetCraftingInventory();
+            IocContainer.RegisterSingleton(craftingInventory);
+        }
+
+        private ICraftingInventory GetCraftingInventory()
+        {
+            var craftingInventoryItems = new[]
+            {
+                new CraftingInventoryItem(0, 0, 0),
+                new CraftingInventoryItem(1, 1, 1),
+                new CraftingInventoryItem(2, 2, 2)
+            };
+            
+            return new CraftingInventory(craftingInventoryItems);
+        }
+        
         public void Start()
         {
             SceneManager.LoadScene("CraftingView");
--- Assets/Scripts/Crawler/UI/Tests/BootstrapperTests.cs
+++ Assets/Scripts/Crawler/UI/Tests/BootstrapperTests.cs
@@ -1,6 +1,19 @@
-﻿namespace Crawler.UI.Tests
+﻿using Crawler.Crafting;
+using DefaultNamespace;
+using NUnit.Framework;
+
+namespace Crawler.UI.Tests
 {
     public class BootstrapperTests
     {
+        [Test]
+        public void When_Awake_Then_CraftingInventoryRegisteredToIocContainer()
+        {
+            var bootstrapper = new Bootstrapper();
+            
+            bootstrapper.Awake();
+            
+            Assert.DoesNotThrow(()=> IocContainer.GetSingleton<ICraftingInventory>());
+        }
     }
 }
