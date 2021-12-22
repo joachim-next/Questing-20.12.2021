2e5d7d75bc15e2df5d72664c88597ad66447dc50
Joachim Mero <joachim.mero@nextgames.com>
2021-12-22 09:34:01 +0200

    Initialize grid presenter in craftingviewmanager

--- Assets/Scripts/Crawler/UI/Crafting/CraftingViewManager.cs
+++ Assets/Scripts/Crawler/UI/Crafting/CraftingViewManager.cs
@@ -1,15 +1,32 @@
+using Crawler.Crafting;
+using DefaultNamespace;
 using UnityEngine;
 
 namespace Crawler.UI.Crafting
 {
     public class CraftingViewManager : MonoBehaviour
     {
+        private ICraftingGridView _gridView;
+        
         private ICraftingGridPresenter _gridPresenter;
      
         // USED ONLY IN TESTS
         public CraftingViewManager(ICraftingGridPresenter gridPresenter)
         {
             _gridPresenter = gridPresenter;
+        }
+
+        public void Awake()
+        {
+            _gridPresenter = CreateGridPresenter();
+        }
+
+        private ICraftingGridPresenter CreateGridPresenter()
+        {
+            var craftingInventory = IocContainer.GetSingleton<ICraftingInventory>();
+            var viewModelConverter = new CraftingInventoryViewModelConverter();
+            
+            return new CraftingGridPresenter(_gridView, craftingInventory, viewModelConverter);
         }
         
         public void Start()
