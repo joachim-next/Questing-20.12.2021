5ca33a9f40f56c7f58d1506a15fa4908f94f4b45
Joachim Mero <joachim.mero@nextgames.com>
2021-12-23 06:47:58 +0200

    Implement view-selecting with mouse

---
+++ Assets/Scripts/Crawler/UI/Crafting/CraftingInventoryItemViewMover.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Crawler.UI.Crafting
+{
+    public class CraftingInventoryItemViewMover : MonoBehaviour
+    {
+        [SerializeField] 
+        private GraphicRaycaster _raycaster;
+        [SerializeField] 
+        private EventSystem _eventSystem;
+
+        private CraftingInventoryItemView _selectedView;
+    
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                SelectView();
+            }
+        }
+
+        private void SelectView()
+        {
+            var view = GetClickedViewOrNull();
+
+            if (view == null)
+            {
+                return;
+            }
+
+            _selectedView = view;
+        }
+
+        private CraftingInventoryItemView GetClickedViewOrNull()
+        {
+            CraftingInventoryItemView view = null;
+
+            var pointerEventData = new PointerEventData(_eventSystem);
+            pointerEventData.position = Input.mousePosition;
+
+            var raycastResults = new List<RaycastResult>();
+
+            _raycaster.Raycast(pointerEventData, raycastResults);
+
+            foreach (var result in raycastResults)
+            {
+                var component = result.gameObject.GetComponent<CraftingInventoryItemView>();
+
+                if (component == null)
+                {
+                    continue;
+                }
+                
+                view = component;
+                break;
+            }
+            
+            return view;
+        }
+    }
+}
